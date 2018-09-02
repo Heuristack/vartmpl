d975f0dc9f89cae37997fd1cdc0c89f1a56586b6
Lance LI <heuristack@gmail.com>
2018-09-02 21:16:29 +0800

    variadic template: add some compile-time predicates

--- vartmpl.h
+++ vartmpl.h
@@ -13,10 +13,15 @@ template <> struct is_all<> : bool_1 {};
 template <int ... Is> struct is_all_even : is_all<true, (Is % 2 == 0)...> {};
 template <bool ... Bs> struct is_all_17 : bool_constant<(Bs && ...)> {};
 template <int ... Is> constexpr bool is_all_even_17() { return ((Is % 2 == 0) && ...); }
+template <typename T, typename ... Ts> struct is_all_same_17 : is_all_17<is_same<T,Ts>::value...> {};
+template <bool ... Bs> struct is_any_17 : bool_constant<!is_all_17<!Bs...>::value> {};
 static_assert(is_all<true,true,true,true>::value == true);
 static_assert(is_all_even<2,4,6,8,10>::value == true);
 static_assert(is_all_even<2,4,5,9,10>::value == false);
 static_assert(is_all_17<true,true,true,true>::value == true);
 static_assert(is_all_even_17<2,4,6,8,10>() == true);
 static_assert(is_all_even_17<2,4,5,9,10>() == false);
+static_assert(is_all_same_17<int,int,int>::value == true);
+static_assert(is_all_same_17<int,int,double>::value == false);
+static_assert(is_any_17<true,false,false>::value == true);
 
